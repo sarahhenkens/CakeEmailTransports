f89d6219a3c0cb352c9cff855d9b4c882c9f8bd9
Jelle Henkens <jelle.henkens@gmail.com>
2012-06-17 00:41:30 +0100

    Preparing test case with helpers

--- Test/Case/Network/Email/PostmarkTransportTest.php
+++ Test/Case/Network/Email/PostmarkTransportTest.php
@@ -12,12 +12,74 @@
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
-App::uses('SmtpTransport', 'CakeEmailTransports.Network/Email');
+App::uses('PostmarkTransport', 'CakeEmailTransports.Network/Email');
+App::uses('HttpSocket', 'Network/Http');
+
+/**
+ * Help to test PostmarkTransport
+ *
+ */
+class PostmarkTestTransport extends PostmarkTransport {
+
+/**
+ * Helper to change the socket
+ *
+ * @param HttpSocket $socket
+ * @return void
+ */
+	public function setSocket(HttpSocket $socket) {
+		$this->_socket = $socket;
+	}
+
+/**
+ * Helper to change the CakeEmail
+ *
+ * @param object $cakeEmail
+ * @return void
+ */
+	public function setCakeEmail($cakeEmail) {
+		$this->_cakeEmail = $cakeEmail;
+	}
+
+/**
+ * Disabled the socket change
+ *
+ * @return void
+ */
+	protected function _generateSocket() {
+	}
+
+/**
+ * Magic function to call protected methods
+ *
+ * @param string $method
+ * @param string $args
+ * @return mixed
+ */
+	public function __call($method, $args) {
+		$method = '_' . $method;
+		return $this->$method();
+	}
+
+}
 
 /**
  * Test case
  *
  */
 class PostmarkTransportTest extends CakeTestCase {
+/**
+ * Setup
+ *
+ * @return void
+ */
+	public function setUp() {
+		if (!class_exists('MockSocket')) {
+			$this->getMock('HttpSocket', array(), array(), 'MockSocket');
+		}
+		$this->socket = new MockSocket();
 
+		$this->PostmarkTransport = new PostmarkTestTransport();
+		$this->PostmarkTransport->setSocket($this->socket);
+	}
 }
