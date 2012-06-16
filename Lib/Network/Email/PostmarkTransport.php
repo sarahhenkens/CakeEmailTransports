2933ba789077e6016f1dbc0a88f47ec68db70d9e
Jelle Henkens <jelle.henkens@gmail.com>
2012-06-16 23:56:32 +0100

    Initial version of the postmark transport

---
+++ Lib/Network/Email/PostmarkTransport.php
@@ -0,0 +1,207 @@
+<?php
+/**
+ * Send mail using the Postmark service
+ *
+ * PHP 5
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2012-2012, Jelle Henkens.
+ * @package       CakeEmailTransports.Network.Email
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+App::uses('AbstractTransport', 'Network/Email');
+App::uses('HttpSocket', 'Network/Http');
+
+class PostmarkTransport extends AbstractTransport {
+
+/**
+ * CakeEmail
+ *
+ * @var CakeEmail
+ */
+	protected $_cakeEmail;
+
+/**
+ * The API endpoint URI.
+ * @var string 
+ */
+	protected $_apiUri = 'api.postmarkapp.com/email';
+
+/**
+ * Holds the data to be sent to the API
+ * @var array 
+ */
+	protected $_data = array();
+
+/**
+ * Content of email to return
+ *
+ * @var array
+ */
+	protected $_content = array();
+
+/**
+ * Set the configuration
+ *
+ * @param array $config
+ * @return void
+ */
+	public function config($config = array()) {
+		$default = array(
+			'apiKey' => false,
+			'secure' => false,
+			'tag' => false
+		);
+
+		$this->_config = $config + $default;
+	}
+
+/**
+ * Send mail
+ *
+ * @param CakeEmail $email CakeEmail
+ * @return array
+ * @throws CakeException
+ */
+	public function send(CakeEmail $email) {
+		$this->_cakeEmail = $email;
+
+		$this->_prepareData();
+		$this->_prepareAttachments();
+		$this->_postmarkSend();
+
+		return $this->_content;
+	}
+
+/**
+ * Prepares the data array.
+ * Adds headers and content
+ *
+ * @return void 
+ */
+	protected function _prepareData() {
+		$this->_data = array();
+
+		$headers = $this->_cakeEmail->getHeaders(array('from', 'sender', 'replyTo', 'returnPath', 'to', 'cc', 'subject'));
+
+		$map = array('From', 'To', 'Cc', 'Bcc', 'Subject');
+		foreach ($map as $header) {
+			if (!empty($headers[$header])) {
+				$this->_data[$header] = $headers[$header];
+			}
+		}
+
+		if (!empty($headers['Reply-To'])) {
+			$this->_data['ReplyTo'] = $headers['Reply-To'];
+		}
+
+		if (isset($headers['X-Tag'])) {
+			$this->_data['Tag'] = $headers['X-Tag'];
+		} elseif (isset($this->_config['tag'])) {
+			$this->_data['Tag'] = $this->_config['tag'];
+		}
+
+		$map = array('X-Mailer', 'MIME-Version', 'Content-Transfer-Encoding');
+		foreach ($map as $header) {
+			if (!empty($headers[$header])) {
+				$this->_addHeader($header, $headers[$header]);
+			}
+		}
+
+		$emailFormat = $this->_cakeEmail->emailFormat();
+
+		if ($emailFormat == 'both' || $emailFormat == 'text') {
+			$this->_data['TextBody'] = $this->_cakeEmail->message('text');
+		}
+
+		if ($emailFormat == 'both' || $emailFormat == 'html') {
+			$this->_data['HtmlBody'] = $this->_cakeEmail->message('html');
+		}
+	}
+
+/**
+ * Reads attached files and adds them to the data
+ *
+ * @return void 
+ */
+	protected function _prepareAttachments() {
+		$attachments = $this->_cakeEmail->attachments();
+
+		if (empty($attachments)) {
+			return;
+		}
+
+		foreach ($attachments as $filename => $info) {
+			$content = $this->_readFile($info['file']);
+			$this->_data['Attachments'][] = array(
+				'Name' => $filename,
+				'Content' => $content,
+				'ContentType' => $info['mimetype']
+			);
+		}
+	}
+
+/**
+ * Reads a file from the filesystem
+ *
+ * @param string $file Absolute path to the file
+ * @return string base64 encoded data 
+ */
+	protected function _readFile($file) {
+		$handle = fopen($file, 'rb');
+		$data = fread($handle, filesize($file));
+		$data = chunk_split(base64_encode($data));
+		fclose($handle);
+		return $data;
+	}
+
+/**
+ * Adds a custom header to the data
+ *
+ * @param string $name
+ * @param string $value
+ * @return void
+ */
+	protected function _addHeader($name, $value) {
+		$this->_data['Headers'][] = array('Name' => $name, 'Value' => $value);
+	}
+	
+/**
+ * Posts the data to the postmark API endpoint
+ *
+ * @return array
+ * @throws CakeException
+ */
+	protected function _postmarkSend() {
+		$protocol = $this->_config['secure'] ? 'https' : 'http';
+		$uri = $protocol . '://' . $this->_apiUri;
+
+		$apiKey = $this->_config['debug'] ? 'POSTMARK_API_TEST' : $this->_config['apiKey'];
+
+		$request = array(
+			'header' => array(
+				'Accept' => 'application/json',
+				'Content-Type' => 'application/json',
+				'X-Postmark-Server-Token' => $apiKey
+			)
+		);
+
+		$socket = new HttpSocket();
+		$return = $socket->post($uri, json_encode($this->_data), $request);
+
+		$response = json_decode($return);
+
+		if ($socket->response->code != '200') {
+			throw new CakeException($response->Message);
+		}
+
+		$this->_content = array(
+			'headers' => array(),
+			'message' => $this->_data,
+			'response' => $response
+		);
+	}
+}
---
+++ Test/Case/Network/Email/PostmarkTransportTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * PostMarkTransportTest file
+ *
+ * PHP 5
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2012-2012, Jelle Henkens.
+ * @package       CakeEmailTransports.Test.Network.Email
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+App::uses('SmtpTransport', 'CakeEmailTransports.Network/Email');
+
+/**
+ * Test case
+ *
+ */
+class PostmarkTransportTest extends CakeTestCase {
+
+}
